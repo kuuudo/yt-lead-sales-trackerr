df6dcba8688ce941f1b30ddd13bb8f90a78558eb
kuuudo <kuuudo@users.noreply.github.com>
2026-06-03 19:47:50 +0700

    add temporarily in useOrganization.ts, delete later

--- src/lib/useOrganization.ts
+++ src/lib/useOrganization.ts
@@ -9,14 +9,20 @@ export function useOrganization() {
 
   useEffect(() => {
     if (!user) return
+
     supabase
       .from('organization_members')
       .select('organization_id')
       .eq('user_id', user.id)
       .eq('role', 'owner')
       .single()
-      .then(({ data }) => {
-        setOrganizationId(data?.organization_id ?? null)
+      .then((result) => {
+        console.log('ORG QUERY RESULT', result)
+
+        setOrganizationId(
+          result.data?.organization_id ?? null
+        )
+
         setLoading(false)
       })
   }, [user])
