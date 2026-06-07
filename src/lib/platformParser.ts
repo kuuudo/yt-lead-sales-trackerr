356495aee06681ba90683d07b31ffe12f9d68044
kuuudo <kuuudo@users.noreply.github.com>
2026-06-07 17:58:33 +0700

    fixplatfrom id facebook

--- src/lib/platformParser.ts
+++ src/lib/platformParser.ts
@@ -285,7 +285,7 @@ export async function getPlatformInfo(
         platform: 'facebook',
         platform_url: cleanUrl,
         platform_post_id: postId,
-        video_title: `${fbLabel} • ${postId}`,
+        video_title: original_title ?? '',
         thumbnail_url: '',
       }
     }
